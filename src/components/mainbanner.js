c1f5d1b7c01ca968ff97120b61a07e2955ac95b5
Kseniia Kondrat <oootlichno@gmail.com>
2025-01-03 16:06:54 -0600

    wip

--- src/components/mainbanner.js
+++ src/components/mainbanner.js
@@ -5,21 +5,33 @@ import shape from "../img/Shape.png";
 const Banner = () => {
   return (
     <div className="main">
-      <img src={shape} alt="Shape Background" className="shape-image" />
+      <div className="shape-container">
+        <img src={shape} alt="Shape Background" className="shape-image" />
+      </div>
       <div className="banner">
         <div className="banner-text">
           <h1>WOWCLEAN by CPG</h1>
           <h2>One Toilet at a Time</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum
+          </p>
           <div className="banner-button">
-            <a href="#quote-form-section" className="quote-button">Get a Quote</a>
+            <a href="#quote-form-section" className="quote-button">
+              Get a Quote
+            </a>
           </div>
         </div>
-        <img src={bannerImage} alt="WOWClean Banner" className="banner-image" />
+        <img
+          src={bannerImage}
+          alt="WOWClean Banner"
+          className="banner-image"
+        />
       </div>
     </div>
   );
-}
-  export default Banner;
+};
+
+export default Banner;
 
  
