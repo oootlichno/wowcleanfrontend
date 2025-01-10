a419aec4819815f680144be626bc9b0ea8e4e196
Kseniia Kondrat <oootlichno@gmail.com>
2025-01-09 19:23:22 -0600

    wip

--- src/components/phonecall.js
+++ src/components/phonecall.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = ({ phone }) => {
   return (
     <a href={`tel:${phone}`} className="phone-number">
-      Call  {phone}
+      CALL  {phone}
     </a>
   );
 };
